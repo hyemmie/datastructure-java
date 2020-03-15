b3dfc4436cac4df25d45e8a48b66c32a56549943
ihyemin <lhm0710@naver.com>
2020-03-15 17:35:46 +0900

    test commit from eclipse

---
+++ test/src/test/Test.java
@@ -0,0 +1,27 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(64.0*3.0);
+		System.out.println(64/3);
+		System.out.println(64%3);
+		System.out.println(10.0/0.0);
+		System.out.println(-10.0/0.0);
+		System.out.println(0.0/0.0);
+
+		
+		int x = 10;
+		System.out.println(x >= 10);
+		System.out.println(x > 10);
+		System.out.println(x < 10);
+		System.out.println(x <= 10);
+		System.out.println("A" instanceof String);
+		
+		
+		
+	}
+
+}
